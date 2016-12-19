a9d3e7c92e180fc30f7940f2d9942f6f71c2efd3
pattysc <pattys@users.noreply.github.com>
2016-12-19 00:58:03 -0500

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,1 +1,19 @@
 #Placeholder for a model
+class Post < ActiveRecord::Base
+  # @@all = []
+  # def initialize(name, content)
+  #   @name = name
+  #   @content = content
+  #   @@all << self
+  # end
+  #
+  # def self.all
+  #   @@all
+  # end
+  #
+  # def self.create(name, content)
+  #   self.new(name, content)
+  #   self.save
+  #   @@all << self
+  # end
+end
